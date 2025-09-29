c0567b444ca8efac8da31aa25ee1ac6571dbd5ca
Malakai <poncemalakai@gmail.com>
2025-09-28 17:27:46 -0700

    Commit 4 - got the main function goin with calls

--- Lab_14.cpp
+++ Lab_14.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class Color {
-    private:        // private stuff that only class members can access
-        int red;
+class Color {           // basically the blueprint
+    private:            // private stuff that only class members can access
+        int red;        
         int green;
         int blue;
 
     public:
     
     Color() : red(0), green(0), blue(0) {}  // Default constructor
-
-    Color(int r, int g, int b) : red(r), green(g), blue(b) {} // More constructor stuff (parameterized constructor)
 
     void setRed(int r) { red = r; }          // Setters - making private stuff changable
     void setGreen(int g) { green = g; }
@@ -25,3 +23,29 @@ class Color {
         cout << "RGB(" << red << ", " << green << ", " << blue << ")" << endl;
     }
 };
+
+int main() {
+                        
+    Color color1;           // Creating the objects
+    Color color2;
+    Color color3;
+
+    color1.setRed(255);     // Setting colors using setters (calling the setters))
+    color1.setGreen(0);
+    color1.setBlue(0);
+
+    color2.setRed(0);
+    color2.setGreen(0);
+    color2.setBlue(255);
+
+    color3.setRed(0);
+    color3.setGreen(255);
+    color3.setBlue(0);
+
+    cout << "Colors:" << endl;  // Printing the colors (calling the print function)
+    color1.print();
+    color2.print();
+    color3.print();
+
+    return 0;
+}
