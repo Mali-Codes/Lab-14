e4f7ce0957a74274583cbecbca79fcc5d79b14fc
Malakai <poncemalakai@gmail.com>
2025-09-28 17:10:48 -0700

    Commit 3 - setters and getters are cool

--- Lab_14.cpp
+++ Lab_14.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 class Color {
-    private:
+    private:        // private stuff that only class members can access
         int red;
         int green;
         int blue;
 
     public:
+    
+    Color() : red(0), green(0), blue(0) {}  // Default constructor
 
+    Color(int r, int g, int b) : red(r), green(g), blue(b) {} // More constructor stuff (parameterized constructor)
+
+    void setRed(int r) { red = r; }          // Setters - making private stuff changable
+    void setGreen(int g) { green = g; }
+    void setBlue(int b) { blue = b; }
+
+    int getRed() const { return red; }       // Getters - making private stuff readable
+    int getGreen() const { return green; }
+    int getBlue() const { return blue; }
+
+    void print() const {                     // Le Print function
+        cout << "RGB(" << red << ", " << green << ", " << blue << ")" << endl;
+    }
 };
